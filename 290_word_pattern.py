bf08527ce6c58392bb0f3def1587223aa435620e
Jakhongir Murodov <jakhongirmurodov@gmail.com>
2022-11-16 00:30:25 +0900

    Easy problem 290 leetcode

---
+++ 290_word_pattern.py
@@ -0,0 +1,44 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        s_list = s.split()
+        pattern_list = [x for x in pattern]
+        
+        if len(s_list) != len(pattern_list):
+            return False
+
+        d1 = {}
+        d2 = {}
+       
+        for i in range(len(pattern)):
+            
+            p = pattern_list[i]
+            q = s_list[i]
+
+            if p in d1:
+                
+                if d1[p] != q:
+                    
+                    return False
+                
+            if q in d2:
+
+                if d2[q] != p:
+                    
+                    return False
+            
+            d1[p] = q
+            d2[q] = p
+
+
+        return True
+            
+
+
+
+        
+        return True
+
+
+
+
+
